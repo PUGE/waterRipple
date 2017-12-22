3627495c387a0de2cd9d33a6b3ba562416c9e1bf
PUGE <100284685@qq.com>
2017-12-22 17:13:00 +0800

    Initial commit

---
+++ WaterRipple.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  .water-ripple(@click.stop="rippleClick") {{text}}
+</template>
+
+<script>
+  export default {
+    props: {
+      text: String
+    },
+    methods: {
+      rippleClick (e) {
+        const target = this.$el
+        const rect = target.getBoundingClientRect()
+        let ripple = target.childNodes[1]
+        if (!ripple) {
+          ripple = document.createElement('span')
+          ripple.className = 'ripple'
+          ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px'
+          target.appendChild(ripple)
+        }
+        ripple.classList.remove('show')
+        const top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop
+        const left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft
+        ripple.style.top = top + 'px'
+        ripple.style.left = left + 'px'
+        ripple.classList.add('show')
+        return false
+      }
+    }
+  }
+</script>
+
+<style lang='less'>
+  .water-ripple {
+    position: relative;
+    display: block;
+    width: 13em;
+    height: 3em;
+    margin: 2em;
+    border: none;
+    outline: none;
+    letter-spacing: .2em;
+    font-weight: bold;
+    background: #dfdfdf;
+    cursor: pointer;
+    overflow: hidden;
+    user-select: none;
+    border-radius: 2px;
+  }
+  .ripple {
+    position: absolute;
+    background: rgba(0,0,0,.15);
+    border-radius: 100%;
+    transform: scale(0);
+    pointer-events: none;
+  }
+  .ripple.show {
+    animation: ripple .75s ease-out;
+  }
+  @keyframes ripple {
+    to {
+      transform: scale(2);
+      opacity: 0;
+    }
+  }
+</style>
